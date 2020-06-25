5a62bb4b4dc701f6ac0e34dc8f7c2a016903505d
papimon <joel610@likelion.org>
2020-06-26 00:38:22 +0900

    little change

--- blogproject/wsgi.py
+++ blogproject/wsgi.py
@@ -8,8 +8,15 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/colokcolok/likelion-8th-final-blog'
+if path not in sys.path:
+    sys.path.append(path)
+
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogproject.settings')
 
